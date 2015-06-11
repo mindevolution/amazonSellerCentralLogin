4309284d44297ff0e3b5bfbd20a70837a83ad8d4
HeZhiLi <zhili850702@gmail.com>
2015-06-11 11:58:09 +0800

    Update README and docs

--- src/Login.php
+++ src/Login.php
@@ -6,7 +6,7 @@ namespace AmazonSellerCentral;
  * Login amazon seller central user php curl
  * Exampel, get the report page 
  * $url = "https://sellercentral.amazon.com/gp/site-metrics/report.html#&cols=/c0/c1/c2/c3/c4/c5/c6/c7/c8/c9/c10/c11/c12&sortColumn=13&filterFromDate=05/10/2015&filterToDate=06/10/2015&fromDate=05/10/2015&toDate=06/10/2015&reportID=102:DetailSalesTrafficBySKU&sortIsAscending=0&currentPage=0&dateUnit=1&viewDateUnits=ALL&runDate=";
- * $report = Curl::getAmazonBackendUrl(Curl::loginSellercentral($username, $password), $url);
+ * $report = Login::getAmazonBackendUrl(Login::loginSellercentral($username, $password), $url);
  */
 class login
 {
